2ce2d4a1eb77541b0e04d570664be1566bcb19eb
Morris Jobke <hey@morrisjobke.de>
2015-04-16 23:59:35 +0200

    first draft

---
+++ do.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import overpy, os, json
+
+
+def parseData(dataList):
+    if not dataList:
+        return
+    for date, streets in dataList.items():
+        print(date, streets)
+
+path = "data/"
+for f in os.listdir(path):
+    print("current file is: " + f)
+    with open(os.path.join(path, f), "r") as fh:
+        parseData(json.load(fh)["results"])
+
